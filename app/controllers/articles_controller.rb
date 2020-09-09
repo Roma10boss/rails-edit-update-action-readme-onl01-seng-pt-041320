4424b35309af6c42a65f1dc0f0dcb54ad91f7b16
Romario Gustave <romagustave10@gmail.com>
2020-09-09 18:15:42 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   def edit
    @article = Article.find(params[:id])
  end
-  
+
  def update
    @article = Article.find(params[:id])
    @article.update(title: params[:article][:title], description: params[:article][:description])
